36fa8b425ad5b665499ded691a557bbad9527769
samyfrey <samyfrey@yahoo.com>
2022-05-06 14:54:17 -0400

    json intro

--- python_sandbox-master/python_sandbox_starter/py_json.py
+++ python_sandbox-master/python_sandbox_starter/py_json.py
@@ -1,1 +1,17 @@
 # JSON is commonly used with data APIS. Here how we can parse JSON into a Python dictionary
+import json 
+
+# Sample JSON getting from API
+userJSON = '{"first_name":"John", "last_name": "Doe", "age": 20}'
+
+#  parse to dictionnary
+
+user = json.loads(userJSON)
+
+# print(user)
+# print(user['first_name'])
+
+#  does the opposite, to transform to JSON format 
+car = {'make': 'Ford', 'model':'Mustang'}
+carJSON = json.dumps(car)
+print(carJSON)
