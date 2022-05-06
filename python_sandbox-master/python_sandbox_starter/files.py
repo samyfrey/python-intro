fe2f4ab11579ecc2ee538e3004dcaccf0ba86fb0
samyfrey <samyfrey@yahoo.com>
2022-05-06 14:49:04 -0400

    files intro

--- python_sandbox-master/python_sandbox_starter/files.py
+++ python_sandbox-master/python_sandbox_starter/files.py
@@ -1,1 +1,24 @@
 # Python has functions for creating, reading, updating, and deleting files.
+
+myFile = open('myfile.txt', 'w')
+
+# get info on the file
+
+print('Name: ', myFile.name)
+print('Is Closed: ', myFile.closed)
+print('Opening Mode: ', myFile.mode)
+
+#  write to file
+
+myFile.write('I love Python')
+myFile.write(' and Javascript')
+myFile.close()
+
+myFile = open('myfile.txt', 'a')
+myFile.write('I also like Javascript')
+myFile.close()
+
+
+myFile = open('myfile.txt', 'r+')
+text = myFile.read(100)
+print(text)
